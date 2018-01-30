66ba842480d7eb379f4011a5be1660b5aa61b92c
theobeuze <theo.beuze@sylpheo.com>
2018-01-30 14:19:49 +0100

    Initial Commit

---
+++ IEat/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  IEat
+//
+//  Created by Poseidon on 30/01/2018.
+//  Copyright © 2018 Theo Beuze. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
